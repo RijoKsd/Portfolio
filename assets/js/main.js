ef82d6ab7efb32bb4e7e67906c24bdc6353a8a3f
Rijo Sebastian <rijobdk@gmail.com>
2023-10-07 11:06:58 +0530

    Deleted some images, Finished service section

--- assets/js/main.js
+++ assets/js/main.js
@@ -114,17 +114,6 @@
     });
   }
 
-  /*--/ Testimonials owl /--*/
-  $("#testimonial-mf").owlCarousel({
-    margin: 20,
-    autoplay: true,
-    autoplayTimeout: 4000,
-    autoplayHoverPause: true,
-    responsive: {
-      0: {
-        items: 1,
-      },
-    },
-  });
+ 
 
 })(jQuery);
